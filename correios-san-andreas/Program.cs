3f9ceb3b7ec3390115c424a7a8e6d9c241c9e00e
Lucas de Melo Lima <lmelo@osas.com.br>
2020-05-06 16:10:05 -0300

    add project files

---
+++ correios-san-andreas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace correios_san_andreas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
