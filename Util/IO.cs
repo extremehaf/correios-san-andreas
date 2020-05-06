e03ebb8ca3170fa422c0137b95e68a8ebcbf3940
Lucas de Melo Lima <lucasdemelolima@gmail.com>
2020-05-06 19:12:23 -0300

    adicionado projeto de teste e funções util

---
+++ Util/IO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Util
+{
+    public static class IO
+    {
+        public static string ReadFileTxt(string arquivo)
+        {
+            if (File.Exists(arquivo) && Path.HasExtension(".txt"))
+            {
+                // Read entire text file content in one string    
+                return File.ReadAllText(arquivo);
+            }
+
+            return null;
+        }
+        public static string[] ReadLinesFileTxt(string arquivo)
+        {
+            if (File.Exists(arquivo) && Path.HasExtension(".txt"))
+            {
+                // Read entire text file content in one string    
+                return File.ReadAllLines(arquivo);
+            }
+
+            return null;
+        }
+
+        public static string WriteFileTxt(string caminho, string conteudo)
+        {
+            var path = @"Data/rotas.txt";
+
+            File.WriteAllText(path, conteudo, Encoding.UTF8);
+
+            return Path.GetFullPath(path);
+        }
+    }  
+}
